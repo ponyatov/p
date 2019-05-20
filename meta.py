2a2b0eb4646e1d32c6cb0a2b682217d37fa7dcda
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-20 09:56:23 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta.py
+++ meta.py
@@ -10,6 +10,9 @@ class Frame:
         return self.dump()
     def dump(self,depth=0,prefix=''):
         tree = self.pad(depth) + self.head(prefix)
+        if not depth: Frame.dumped = []
+        if self in Frame.dumped: return tree + ' _/'
+        else: Frame.dumped.append(self)
         for i in self.slot:
             tree += self.slot[i].dump(depth+1,i+' = ')
         for j in self.nest:
@@ -28,6 +31,11 @@ class Frame:
         self.nest.append(that)
     def __lshift__(self,that):
         self.slot[that.val] = that ; return self
+        
+class Primitive(Frame): pass
+
+class String(Primitive): pass
+class Symbol(Primitive): pass
 
 class IO(Frame): pass
 
@@ -43,7 +51,11 @@ dir         = Dir('~/' + module.val)    ; module // dir
 project     = File('.project')          ; dir // project
 pydev       = File('.pydevproject')     ; dir // pydev
 gitignore   = File('.gitignore')        ; dir // gitignore
-readme      = File('README.md')         ; module // readme
+
+readme      = File('README.md')         ; module // readme  ; readme // module
+title   = String('homoiconic metaPython')                   ; readme // title
+author  = String('Dmitry Ponyatov <<dponyatov@gmail.com>>') ; readme // author      
+
 py          = File('meta.py')           ; module // py
 settings    = Dir('.settings')          ; dir // settings
 settings    // File('org.eclipse.core.runtime.prefs')
