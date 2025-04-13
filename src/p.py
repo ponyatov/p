cf1f13e13c3d60f11ca62bed12295c47a20ef3e6
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-13 23:54:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/p.py
+++ src/p.py
@@ -6,3 +6,8 @@ for d in dirs:
     try: os.mkdir(d)
     except FileExistsError: pass
     with open(f'{d}/.gitignore','w') as giti: print('!.gitignore',file=giti)
+
+files = ['README.md','LICENSE','Makefile','apt.Debian','.clang-format','.doxygen']
+
+for f in files:
+    with open(f,'a') as ff: pass
